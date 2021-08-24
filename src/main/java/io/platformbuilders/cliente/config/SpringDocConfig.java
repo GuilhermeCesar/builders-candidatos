f64104f52eeaadf5c7ec29e9089962f54bcdbdbf
Guilherme Medeiros <guilherme.medeiros@southsystem.com.br>
2021-08-24 14:24:21 -0300

    Configurar openapi

---
+++ src/main/java/io/platformbuilders/cliente/config/SpringDocConfig.java
@@ -0,0 +1,25 @@
+package io.platformbuilders.cliente.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.swagger-ui.server.list}") final List<String> servers) {
+        return new OpenAPI()
+                .servers(servers.stream().map(s -> new Server().url(s)).toList())
+                .info(new Info().title("Client Api")
+                        .description("Client Api")
+                        .version("1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
--- src/main/java/io/platformbuilders/cliente/resource/v1/ClienteResource.java
+++ src/main/java/io/platformbuilders/cliente/resource/v1/ClienteResource.java
@@ -4,10 +4,14 @@ import io.platformbuilders.cliente.dto.ClienteDTO;
 import io.platformbuilders.cliente.dto.ClienteRequestDTO;
 import io.platformbuilders.cliente.dto.ClienteSearchDTO;
 import io.platformbuilders.cliente.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,8 +24,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Tag(name = "Clientes")
 @Validated
 @RequiredArgsConstructor
 @RestController
@@ -30,24 +37,38 @@ public class ClienteResource {
 
     private final ClienteService clienteService;
 
+    @Operation(summary = "Create client",
+            responses = {@ApiResponse(responseCode = "201", content = @Content(schema = @Schema(implementation = ClienteDTO.class)))}
+    )
     @PostMapping
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(CREATED)
     public ClienteDTO criarCliente(@Valid @RequestBody ClienteRequestDTO clienteRequestDTO) {
         return this.clienteService.salvar(clienteRequestDTO);
     }
 
+    @Operation(summary = "Update client by id",
+            responses = {@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ClienteDTO.class)))})
     @PutMapping("/{clienteId}")
+    @ResponseStatus(OK)
     public ClienteDTO atualizarCliente(@PathVariable("clienteId") Long clienteId,
                                        @Valid @RequestBody ClienteRequestDTO clienteRequestDTO) {
         return this.clienteService.atualizar(clienteId, clienteRequestDTO);
     }
 
+    @Operation(summary = "Get clients",
+            responses = {@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ClienteDTO.class)))}
+    )
     @GetMapping("/{clienteId}")
+    @ResponseStatus(OK)
     public ClienteDTO getCliente(@PathVariable("clienteId") Long clienteId) {
         return this.clienteService.buscar(clienteId);
     }
 
+    @Operation(summary = "Get clients",
+            responses = {@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = Page.class)))}
+    )
     @GetMapping
+    @ResponseStatus(OK)
     public Page<ClienteDTO> getCliente(ClienteSearchDTO clienteDTO, Pageable pageable) {
         return this.clienteService.buscar(clienteDTO, pageable);
     }
